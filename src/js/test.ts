8cdf2d290436914512f7de21e4896078a0b0d3dd
Zeydz <39154045+Zeydz@users.noreply.github.com>
2024-03-20 11:09:21 +0100

    Lagt till addCourse-funktion

--- src/js/test.ts
+++ src/js/test.ts
@@ -1,8 +1,32 @@
 // Interface for course information
+
+const codeInputEl = document.getElementById("code") as HTMLInputElement;
+const nameInputEl = document.getElementById("name") as HTMLInputElement;
+const progInputEl = document.getElementById("progression") as HTMLInputElement;
+const syllabusInputEl = document.getElementById("syllabus") as HTMLInputElement;
+
 interface CourseInfo {
     code: string;
     name: string;
-    progression: 'A' | 'B' | 'C';
+    progression: string;
     syllabus: string;
+}
+
+//Funktion för att lägga till en ny kurs
+function addCourse(course: CourseInfo): void {
+    //Ladda in sparade kurser från localStorage
+    let courses: CourseInfo[] = JSON.parse(localStorage.getItem('courses') || '[]');
+
+    if (courses.some(c => c.code === course.code)) {
+        console.error('Kurskoden existerar redan. Försök igen med en annan kod.')
+        return;
+    }
+
+    //Lägg till kursen till listan
+    courses.push(course);
+
+    // Uppdatera localStorage med den nya listan
+    localStorage.setItem('courses', JSON.stringify(courses));
+    console.log('Lagt till kurs i listan');
 }
 
